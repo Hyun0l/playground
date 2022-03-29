a6d3b725d992020c5deaad2d41d05bebf9171545
Hyun0l <101308526+Hyun0l@users.noreply.github.com>
2022-03-29 15:50:58 +0900

    기본화면구성

--- OddEvenGame/OddEvenGame/ViewController.swift
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var computerBallCountLbl: UILabel!
+    @IBOutlet weak var userBallCountLbl: UILabel!
+    
+    var compBallsCount: Int = 20
+    var userBallsCount: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        computerBallCountLbl.text = String(compBallsCount)
+        userBallCountLbl.text = String(userBallsCount)
+        
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func gameStartPressed(_ sender: Any) {
+        print("게임시작!!")
+    }
+    
 
 }
 
